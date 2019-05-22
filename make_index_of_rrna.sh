7fc1b093770f9b1c9862717e16a4e3d41146086c
Rohit Suratekar <rohitsuratekar@gmail.com>
2019-05-22 17:30:41 +0200

    Updated Local Variables
    Use of base constant to generate relative path

--- make_index_of_rrna.sh
+++ make_index_of_rrna.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 #Creates index files from given rRNA fasta files
 
-echo Script Started at : $(date)
+source ./config.sh
 
 # Path of folder where you want your fasta files and index files to be stored
-readonly rna_path="/home/rsuratekar/Documents/Rohit/Genome/rrna/"
+readonly rna_path="$GENOME_HOME/rrna/"
 
 # Index files will be stored here
 mkdir ${rna_path}index
 
 # Path of SortMeRNA folder
-readonly tool_path="/home/rsuratekar/Documents/Rohit/Tools/sortmerna-3.0.3/"
+readonly tool_path="$TOOL_SORTMERNA/"
 
 # Assuming all rRNA database is in 'rRNA_databases' folder
 find "${tool_path}rRNA_databases/" -name "*.fasta"|
 while read f; 
 do	
-	cp $f $rna_path
-	name=$(echo $f| xargs -I {} basename {})
-	${tool_path}bin/indexdb --ref $f,${rna_path}index/${name}.idx -v
+	cp ${f} ${rna_path}
+	name=$(echo ${f}| xargs -I  basename )
+	${tool_path}bin/indexdb --ref ${f},${rna_path}index/${name}.idx -v
 done
